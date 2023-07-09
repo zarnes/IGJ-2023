3edaa714eeeae629112dab247cd8ed394c81a77b
zarnes <zarnes@live.com>
2023-07-09 10:55:46 +0200

    New project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
 import './assets/main.css'
 
-new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
